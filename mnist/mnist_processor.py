6a1fd976a2f73a01bc8ba16c22a4fa643ef48a6f
spec2e <speconsult@gmail.com>
2017-01-04 10:38:36 +0100

    works

--- mnist/mnist_processor.py
+++ mnist/mnist_processor.py
@@ -1,10 +1,9 @@
-# Standard scientific Python imports
 import matplotlib.pyplot as plt
 import numpy
 
-# Import datasets, classifiers and performance metrics
-from sklearn import datasets, svm, metrics
-
+from sklearn.ensemble import RandomForestClassifier
+from sklearn import metrics
+from sklearn.externals import joblib
 from mnist.mnist_reader import read_data_sets
 
 def flatten_images(images):
@@ -27,26 +26,29 @@ data = flatten_images(train_images)
 
 validation_data = flatten_images(validation_images)
 
-# Create a classifier: a support vector classifier
-classifier = svm.SVC(gamma=0.001)
-first = 4000
-range = 3000
-classifier.fit(data[first: first + range], train_labels[first:first + range])
+classifier = RandomForestClassifier(n_estimators=10)
+classifier.fit(data, train_labels)
 
-idx = 41
-image = validation_images[idx]
+joblib.dump(classifier, 'mnist.pkl')
+
+idx = 568
 expected = validation_labels[idx]
 predicted = classifier.predict([validation_data[idx]])
 
-print('expected %s' % (expected))
-print('predicted %s' % predicted)
+#print('expected %s' % expected)
+#print('predicted %s' % predicted)
 
-def show_image(image, label):
+print("Classification report for classifier %s:\n%s\n"
+      % (classifier, metrics.classification_report([expected], predicted)))
+print("Confusion matrix:\n%s" % metrics.confusion_matrix([expected], predicted))
+
+
+def show_image(image, label, predicted):
     plt.subplot(2, 4, label)
     plt.axis('off')
     plt.imshow(image, cmap=plt.cm.gray_r, interpolation='nearest')
-    plt.title('Expected: %s' % label)
+    plt.title('Expected: ' + str(label) + ', predicted: ' + str(predicted))
     plt.show()
 
-show_image(image, expected)
 
+show_image(validation_images[idx], expected, predicted)
