386e215bf7ab45cc2245a9a6bbd562c149ae15fd
spec2e <speconsult@gmail.com>
2017-01-06 10:24:25 +0100

    predicting wines :-)

--- wine/wine_reader.py
+++ wine/wine_reader.py
@@ -1,19 +1,29 @@
 import pandas as pd
-from sklearn.utils import shuffle
 
 
 def read_wines():
-    data = pd.read_csv('data/wine_normalized.csv', sep=',', header=None)
-    data = shuffle(data)
+    data = read_and_sample()
+
     wine_data = get_data(data)
     labels = get_labels(data)
+    print(labels)
     return [wine_data, labels]
+
+
+def read_and_sample():
+    # data = pd.read_csv('data/wine_normalized.csv', sep=',', header=None)
+    data = pd.read_csv('data/wine.csv', sep=',', header=None)
+    sampled = data.sample(frac=1.0)
+    sampled.to_csv('data/sampled.csv', sep=',', header=None, index=False)
+    data = pd.read_csv('data/sampled.csv', sep=',', header=None)
+    return data
 
 
 def get_labels(wine_data):
     ret_labels = list()
     for index in range(0, len(wine_data)):
-        ret_labels.append(wine_data.ix[index][13])
+        # ret_labels.append(wine_data.ix[index][13])
+        ret_labels.append(wine_data.ix[index][0])
 
     return ret_labels
 
@@ -21,7 +31,7 @@ def get_labels(wine_data):
 def get_data(wine_data):
     ret_data = list()
     for x in range(0, len(wine_data)):
-        record = wine_data.ix[x][0:-1]
-        ret_data.append(record)
+        # ret_data.append(wine_data.ix[x][0:-1])
+        ret_data.append(wine_data.ix[x][1:])
 
     return ret_data
