c0b484637a2d8ef1b215aad56ed2c96fda4742d5
spec2e <speconsult@gmail.com>
2017-01-18 10:22:24 +0100

    added find faces

---
+++ boston/boston_processor.py
@@ -0,0 +1,4 @@
+from sklearn import datasets
+
+boston = datasets.load_boston()
+print(boston)
--- facerecognition/find_faces.py
+++ facerecognition/find_faces.py
@@ -4,7 +4,6 @@ from skimage import io
 # Create a HOG face detector using the built-in dlib class
 face_detector = dlib.get_frontal_face_detector()
 
-#win = dlib.image_window()
 
 # Load the image into an array
 image = io.imread("many_faces.jpg")
@@ -21,14 +20,5 @@ print("I found {} faces".format(len(detected_faces)))
 # Loop through each face we found in
 #  the image
 for i, face_rect in enumerate(detected_faces):
-    # Detected faces are returned as an object with the coordinates
-    # of the top, left, right and bottom edges
-    #print("- Face #{} found at Left: {} Top: {} Right: {} Bottom: {}".format(i, face_rect.left(), face_rect.top(),
-    #                                                                       face_rect.right(), face_rect.bottom()))
     crop = image[face_rect.top():face_rect.bottom(), face_rect.left():face_rect.right()]
     io.imsave("found_faces/img_" + str(i) + ".jpg", crop)
-    # Draw a box around each face we found
-#    win.add_overlay(face_rect)
-
-# Wait until the user hits <enter> to close the window
-dlib.hit_enter_to_continue()
