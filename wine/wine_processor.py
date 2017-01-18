959c70f8a86f5439fafc8120ed0ba5c5c97c350f
spec2e <speconsult@gmail.com>
2017-01-18 19:19:57 +0100

    clean up

--- wine/wine_processor.py
+++ wine/wine_processor.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy
 
+from sklearn.preprocessing import Normalizer
+from numpy import set_printoptions
 from sklearn import metrics
 from sklearn.ensemble.forest import RandomForestClassifier
 from sklearn.linear_model.base import LinearRegression
@@ -15,19 +17,21 @@ END_OF_DATA = 177
 
 def main():
     wines = read_wines()
-
     wines_data = wines[0]
     wines_labels = wines[1]
 
-    classifier = train(wines_data[0:TRAIN_COUNT], wines_labels[0:TRAIN_COUNT])
+    scaler = Normalizer().fit(wines_data)
+    normalized_wines = scaler.transform(wines_data)
 
-    predicted_result = predict(classifier, wines_data[TRAIN_COUNT:END_OF_DATA])
+    classifier = train(normalized_wines[0:TRAIN_COUNT], wines_labels[0:TRAIN_COUNT])
+
+    predicted_result = predict(classifier, normalized_wines[TRAIN_COUNT:END_OF_DATA])
 
     print_results(classifier, wines_labels[TRAIN_COUNT:END_OF_DATA], predicted_result)
 
 
 def train(data, labels):
-    trained_classifier = RandomForestClassifier(n_estimators=10)
+    trained_classifier = RandomForestClassifier(n_estimators=10, random_state=7)
     #trained_classifier = GaussianNB()
     #trained_classifier = MLPClassifier()
     #trained_classifier = LinearRegression()
@@ -47,12 +51,5 @@ def print_results(classifier, expected, predicted):
           % (classifier, metrics.classification_report(expected, predicted)))
     print("Confusion matrix:\n%s" % metrics.confusion_matrix(expected, predicted))
 
-    y = numpy.array(expected)
-    fig, ax = plt.subplots()
-    ax.scatter(expected, predicted)
-    ax.plot([y.min(), y.max()], [y.min(), y.max()], 'k--', lw=4)
-    ax.set_xlabel('Measured')
-    ax.set_ylabel('Predicted')
-    #plt.show()
 
 main()
--- wine/wine_reader.py
+++ wine/wine_reader.py
@@ -2,19 +2,15 @@ import pandas as pd
 
 
 def read_wines():
-    data = read_and_sample()
+    data = read()
 
     wine_data = get_data(data)
     labels = get_labels(data)
     return [wine_data, labels]
 
 
-def read_and_sample():
+def read():
     data = pd.read_csv('data/wine_normalized.csv', sep=',', header=None)
-    #data = pd.read_csv('data/wine.csv', sep=',', header=None)
-    sampled = data.sample(frac=1.0)
-    sampled.to_csv('data/sampled.csv', sep=',', header=None, index=False)
-    data = pd.read_csv('data/sampled.csv', sep=',', header=None)
     return data
 
 
@@ -31,6 +27,5 @@ def get_data(wine_data):
     ret_data = list()
     for x in range(0, len(wine_data)):
         ret_data.append(wine_data.ix[x][0:-1])
-        #ret_data.append(wine_data.ix[x][1:])
 
     return ret_data
