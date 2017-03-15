c083067131afd078b9bdac9ec7e95bab72d60230
spec2e <speconsult@gmail.com>
2017-03-15 15:15:46 +0100

    ques_answ

--- qa/question_answer.py
+++ qa/question_answer.py
@@ -1,10 +1,14 @@
 import json
 import ast
+import numpy
+import gensim
 from sklearn.model_selection import train_test_split
+from keras.preprocessing import sequence
+
 
 all_q_a = []
 X = []
-Y = []
+y = []
 
 with open("qa_baby.json") as f:
 
@@ -18,15 +22,15 @@ with open("qa_baby.json") as f:
 print("number of question/answers %i" % len(all_q_a))
 
 for qa in all_q_a:
-    X.append(qa["question"])
-    Y.append(qa["answer"])
+    X.append(qa["question"].lower().split())
+    y.append(qa["answer"].lower().split())
 
 
 print("count in X %i" % len(X))
-print("count in Y %i" % len(Y))
+print("count in Y %i" % len(y))
 
 
-x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.3)
+x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
 x_test, x_val, y_test, y_val = train_test_split(x_test, y_test, test_size=0.5)
 
 print("count x_train %i" % len(x_train))
@@ -37,3 +41,11 @@ print("count x_val %i" % len(x_val))
 print("count y_val %i" % len(y_val))
 
 
+top_words = 5000
+
+max_words_question = 300
+
+
+model = gensim.models.Word2Vec(X, size=100, min_count=10, iter=1)
+res = model.most_similar(positive=['car', 'seat'], topn=1)
+print(res)
