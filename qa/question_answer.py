c4e8b41cac3a822a5bdc594a11b3106522b2ede3
spec2e <speconsult@gmail.com>
2017-03-15 12:30:41 +0100

    ques_answ

--- qa/question_answer.py
+++ qa/question_answer.py
@@ -1,7 +1,10 @@
 import json
 import ast
+from sklearn.model_selection import train_test_split
 
 all_q_a = []
+X = []
+Y = []
 
 with open("qa_baby.json") as f:
 
@@ -12,5 +15,25 @@ with open("qa_baby.json") as f:
         all_q_a.append(json_data)
 
 
+print("number of question/answers %i" % len(all_q_a))
+
+for qa in all_q_a:
+    X.append(qa["question"])
+    Y.append(qa["answer"])
+
+
+print("count in X %i" % len(X))
+print("count in Y %i" % len(Y))
+
+
+x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.3)
+x_test, x_val, y_test, y_val = train_test_split(x_test, y_test, test_size=0.5)
+
+print("count x_train %i" % len(x_train))
+print("count y_train %i" % len(y_train))
+print("count x_test %i" % len(x_test))
+print("count y_test %i" % len(y_test))
+print("count x_val %i" % len(x_val))
+print("count y_val %i" % len(y_val))
 
 
